f23f73accc788e3b52afdeb80a275629553685a2
Basanagoud <Basanagoud.SP@sunmobility.com>
2023-12-29 16:30:35 +0530

    created owner and customer login screen

--- prov-store/src/app/app-routing.module.ts
+++ prov-store/src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OwnerLoginComponent } from './owner-login/owner-login.component';
+import { CustomerLoginComponent } from './customer-login/customer-login.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: MainScreenComponent
+  },
+  {path:'main-screen', component:MainScreenComponent},
+  {
+    path:'owner-login', component:OwnerLoginComponent
+  },
+  {
+    path:'customer-login', component:CustomerLoginComponent
+  }
+];
 
-@NgModule({
+@NgModule({     
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
