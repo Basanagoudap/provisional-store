09b9b6c0c669cc5abb07b59aaa025f06f732e96b
Shaik Mohammed Subhan <subhan.shaik@sunmobility.com>
2024-01-02 14:27:52 +0530

    added customer model

--- prov-apis/app.js
+++ prov-apis/app.js
@@ -4,6 +4,7 @@ let app = express();
 let mongoose = require("mongoose");
 let cors = require("cors");
 let owners = require('./models/owner');
+let customers = require('./models/customer');
 require('dotenv').config();
 const dbStr = process.env.DATABASE_STR;
 
@@ -23,7 +24,7 @@ database.once("connected", () => {
   console.log("Database Connected");
 });
 
-app.post('/post', jsonParser, async (req, res) => {
+app.post('/owner/post', jsonParser, async (req, res) => {
     try {
         const data = new owners({
             name: req.body.name,
@@ -38,9 +39,29 @@ app.post('/post', jsonParser, async (req, res) => {
         res.status(400).json({message: error.message})
     }
 })
-app.get('/getData', (req, res) => {
-    
+app.get('/owner/getData', (req, res) => {
+  
 })
+
+app.post('/customer/post', jsonParser, async (req, res) => {
+    try {
+        const data = new owners({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password, 
+            address: req.body.address, 
+            cart: req.body.cart, 
+            pastOrders: req.body.pastOrders, 
+            pendingOrders: req.body.pendingOrders
+        })
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
